2b402b56e39a078d3871743b30496ed56ad4840e
maxter <me@maxters.net>
2016-01-09 03:18:37 +0400

    source dir from gradle model

--- Android.mk
+++ Android.mk
@@ -5,10 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := XposedBridge
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(call all-java-files-under, HiddenAPIStubs/src)
-LOCAL_SRC_FILES += $(call all-java-files-under, lib/apache-commons-lang)
+LOCAL_SRC_FILES := $(call all-java-files-under, hiddenapistubs/src/main/java/)
+LOCAL_SRC_FILES += $(call all-java-files-under, app/src/main/apacheCommonsLang/external/)
 
-LOCAL_SRC_FILES += $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under, app/src/main/java/)
 LOCAL_PROGUARD_ENABLED := disabled
 include $(BUILD_JAVA_LIBRARY)
 
